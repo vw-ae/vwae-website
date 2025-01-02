840c665b84a91487fd253def1f7d2ccd0b1367b3
brunobritorj <brunobritorj@outlook.com>
2025-01-02 20:06:36 -0300

    Fix subpath with custom domain

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={basePath ? `${basePath}/${src}` : src} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={basePath ? `${basePath}/${picture}` : src} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
