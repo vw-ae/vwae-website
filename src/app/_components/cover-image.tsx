a23736e6d7bdd5b8c3ef18159704a645ab1b213c
brunobritorj <brunobritorj@outlook.com>
2025-01-02 20:00:13 -0300

    Fix subpath with custom domain

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -14,7 +14,7 @@ type Props = {
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
     <Image
-      src={`${basePath}/${src}`}
+      src={basePath ? `${basePath}/${src}` : src}
       alt={`Cover Image for ${title}`}
       className={cn("shadow-sm w-full", {
         "hover:shadow-lg transition-shadow duration-200": slug,
